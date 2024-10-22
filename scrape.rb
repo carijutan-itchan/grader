5f9a457257d8eb1c76c469bec5595300ef788dd7
Christian-Baman <carijutan.christian@gmail.com>
2024-10-22 18:23:14 +0800

    Add functionality that display the student list as table

--- scrape.rb
+++ scrape.rb
@@ -4,9 +4,15 @@ require 'pry'
 
 file = File.open("./files/grades.csv")
 grader = Grader.new(file)
+grader.parse_student
 
 table = Terminal::Table.new do |t|
-  t << ["Sample", "Output", "Here"]
+  t << ["Name", "English", "Math", "Physics", "Average"]
+
+  grader.students.each do |student|
+    t.add_separator
+    t.add_row([student.full_name, student.english, student.math, student.physics, student.average])
+  end
 end
 
 puts table
